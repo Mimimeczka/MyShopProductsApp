eaef4c82582e8463ecac3a3f9229021eb61fe27e
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-08 14:33:57 +0200

    MySQL connection

--- MyProducts/MyProducts/settings.py
+++ MyProducts/MyProducts/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MyProducts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ProductsApp',
+        'USER': 'ProductsUser',
+        'PASSWORD': 'ProductsUser',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
