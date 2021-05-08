3be103aff53dc1562a4ce81eff93021e3c6d45c4
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-08 15:57:39 +0200

    create serializer and viewset for Product model

--- MyProducts/ProductsApp/views.py
+++ MyProducts/ProductsApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializers import ProductSerializer
+from rest_framework import viewsets
+from .models import Product
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
