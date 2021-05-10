fecb5d82fac48d7c3b48a38df2edba71c92b5c63
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-10 16:40:38 +0200

    create metods for Product model: qet_queryset, list, retrive, create, update, destroy.

--- MyProducts/ProductsApp/urls.py
+++ MyProducts/ProductsApp/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from ProductsApp import views
 
 router = routers.DefaultRouter()
-router.register(r'products', views.ProductViewSet)
+router.register(r'products', views.ProductViewSet, basename='Product')
 
 urlpatterns = [
     path('', include(router.urls))
--- MyProducts/ProductsApp/views.py
+++ MyProducts/ProductsApp/views.py
@@ -1,8 +1,49 @@
 from .serializers import ProductSerializer
 from rest_framework import viewsets
 from .models import Product
+from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        product = Product.objects.all()
+        return product
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = ProductSerializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        product = Product.objects.create(
+            name=request.data['name'],
+            description=request.data['description'],
+            price=request.data['price']
+        )
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        product = self.get_object()
+        if request.data['name'] is not '':
+            product.name = request.data['name']
+        if request.data['description'] is not '':
+            product.description = request.data['description']
+        if request.data['price'] is not '':
+            product.price = request.data['price']
+        product.save()
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        product = self.get_object()
+        product.delete()
+        return Response('Product removed')
